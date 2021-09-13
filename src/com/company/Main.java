0e661d718963663b118cde4dda4ae03e5778ea58
Pragnya Yadav <pragnyayadav22@gmail.com>
2021-09-13 19:43:54 +0530

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void my1(){
+        System.out.println("I cant handle thissss");
+    }
+
+    public static void my2(int x){
+        System.out.println("Speed is " + x);
+    }
+}
